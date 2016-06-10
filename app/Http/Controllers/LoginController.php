a33783b1971ec05a7b19febf589fe38aadc89b30
Greg Matthews <gregfmatthews@gmail.com>
2016-06-10 11:35:03 -0500

    clean up

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,24 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class LoginController extends Controller
 {
+    
 
-    Event::listen
-    Event::listen('Aacotroneo\Saml2\Events\Saml2LoginEvent', function (Saml2LoginEvent $event) {
-
-    $user = $event->getSaml2User();
-    $userData = [
-        'id' => $user->getUserId(),
-        'attributes' => $user->getAttributes(),
-        'assertion' => $user->getRawSamlAssertion()
-    ];
-    $laravelUser = //find user by ID or attribute
-        //if it does not exist create it and go on  or show an error message
-        Auth::login($laravelUser);
-    });
 }
