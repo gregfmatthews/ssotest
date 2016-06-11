9641688cd4df36bb38a615296adc576832b52817
Greg Matthews <gregfmatthews@gmail.com>
2016-06-11 11:04:51 -0500

    disabled cache

--- app/Authentication/UserProvider.php
+++ app/Authentication/UserProvider.php
@@ -18,7 +18,7 @@ class UserProvider implements IlluminateUserProvider
     public function retrieveById($identifier)
     {
 
-        return Cache::remember('userById_'.$identifier,60, function() use ($identifier) {
+        //return Cache::remember('userById_'.$identifier,60, function() use ($identifier) {
             $id = $identifier == 334116 ? 42968 : $identifier;
             logger()->info('fetching');
             $response = Guzzle::post(Config('kwuapi.getmarketcenterurl'), [
@@ -31,7 +31,7 @@ class UserProvider implements IlluminateUserProvider
             $user->marketcenters = $marketcenters;
 
             return $user;
-        });
+        //});
 
     }
     public function updateRememberToken(Authenticatable $user, $token)
