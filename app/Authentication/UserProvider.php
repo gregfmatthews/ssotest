2599567b202bc0cd94d28dd896efa28cb673374d
Greg Matthews <gregfmatthews@gmail.com>
2016-06-11 10:18:27 -0500

    fixed cache lookup

--- app/Authentication/UserProvider.php
+++ app/Authentication/UserProvider.php
@@ -18,7 +18,7 @@ class UserProvider implements IlluminateUserProvider
     public function retrieveById($identifier)
     {
 
-        return Cache::remember('userById_',$identifier,60, function() use ($identifier) {
+        return Cache::remember('userById_'.$identifier,60, function() use ($identifier) {
             $id = $identifier == 334116 ? 42968 : $identifier;
             logger()->info('fetching');
             $response = Guzzle::post(Config('kwuapi.getmarketcenterurl'), [
