833011fddafbe55dc153a151ca5e4b0e4e277dc4
Greg Matthews <gregfmatthews@gmail.com>
2016-06-11 13:30:45 -0500

    ns fix

--- app/Authentication/AuthServiceProvider.php
+++ app/Authentication/AuthServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace App\Authentication;
 use Auth;
-use Illuminate\Auth\Access\Gate as GateContract;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     public function boot(GateContract $gate){
+
         $this->registerPolicies($gate);
 
 
