248c462e0519096a4dc4f4bb08841e1ea38cbcec
Greg Matthews <gregfmatthews@gmail.com>
2016-06-11 09:08:21 -0500

    test

--- app/Authentication/UserProvider.php
+++ app/Authentication/UserProvider.php
@@ -23,8 +23,8 @@ class UserProvider implements IlluminateUserProvider
         $marketcenters = json_decode($response->getBody(1));
         $user = new User();
         $user->kwuid = $identifier;
-        $user->markercenters = $marketcenters;
-        
+        //$user->markercenters = $marketcenters;
+
         return $user;
     }
     public function updateRememberToken(Authenticatable $user, $token)
