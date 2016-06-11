f8d329b3319f53dd704f0b9a34e35ea32a1f20ef
Greg Matthews <gregfmatthews@gmail.com>
2016-06-10 21:12:54 -0500

    added provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use App\Extensions\KwuUserProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
