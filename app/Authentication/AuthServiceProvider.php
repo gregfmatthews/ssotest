90aa9fb796acb887f86b5775be1823a03a4556a8
Greg Matthews <gregfmatthews@gmail.com>
2016-06-11 13:25:25 -0500

    Gate test

--- app/Authentication/AuthServiceProvider.php
+++ app/Authentication/AuthServiceProvider.php
@@ -2,11 +2,29 @@
 
 namespace App\Authentication;
 use Auth;
+use Illuminate\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    public function boot(){
+    public function boot(GateContract $gate){
+        $this->registerPolicies($gate);
+
+
+        $gate->define('view-dash',function($user,$id){
+            if($user->kwuid==$id){
+                return true;
+            }
+            if(!empty($user->marketcenters)){
+                return true;
+            }
+        });
+
+        $gate->define('view-admin',function($user){
+            if(!empty($user->marketcenters)){
+                return true;
+            }
+        });
 
         Auth::provider('our_provider',function($app, array $config){
             return new UserProvider();
--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Auth;
-
+use Gate;
 class DashboardController extends Controller
 {
     public function index(Request $request){
@@ -18,7 +18,14 @@ class DashboardController extends Controller
         }
     }
 
-    public function test(){
-        die('test');
+    public function test($id=null){
+        $user = Auth::guard('web')->user();
+        if(!$id){
+            $id=$user->kwuid;
+        }
+        if(Gate::denies('view-dash',$id)){
+            abort(403);
+        }
+        dd('can view');
     }
 }
